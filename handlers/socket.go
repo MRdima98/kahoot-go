ed94552613c6dd223fb7bdd281c994d1ce6c2a7b
dumitru <dumitru.frunza@widegroup.eu>
2025-02-26 14:53:30 +0100

    Player connection management

--- handlers/socket.go
+++ handlers/socket.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"text/template"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/redis/go-redis/v9"
@@ -26,22 +28,33 @@ const (
 	playersKey         = "players"
 )
 
-type player struct {
+type Player struct {
 	Name   string `json:"name"`
 	Status string `json:"status"`
 	Score  int    `json:"score"`
 }
 
 func SocketHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("\n\nOpened new connection!")
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	fmt.Println("conn: ", conn)
 
+	var player Player
 	rdb := redisClient()
-	fmt.Println("We hit it")
+
+	conn.SetCloseHandler(func(code int, text string) error {
+		if player == (Player{}) {
+			return errors.New("No player on this connection...somehow")
+		}
+
+		savePlayerInfo(player, rdb, disconnected)
+
+		rdb.Close()
+		return nil
+	})
 
 	for {
 		_, p, err := conn.ReadMessage()
@@ -53,44 +66,18 @@ func SocketHandler(w http.ResponseWriter, r *http.Request) {
 		var result map[string]any
 		err = json.Unmarshal(p, &result)
 		if err != nil {
-			fmt.Println("Error unmarshaling JSON:", err)
+			fmt.Println("Error unmarshaling JSON in for loop get:", err)
 			return
 		}
 
 		if result["name"] != nil {
-			player := player{
+			player = Player{
 				Name:   result["name"].(string),
 				Status: connected,
 				Score:  0,
 			}
-			fmt.Println("player obj: ", player)
-
-			playerJSON, err := json.Marshal(player)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println("My JSON: ", playerJSON)
-
-			err = rdb.Set(ctx, player.Name, playerJSON, 0).Err()
-			if err != nil {
-				log.Println(err)
-			}
+			savePlayerInfo(player, rdb, connected)
 		}
-
-		val, err := rdb.Get(ctx, "John").Result()
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Println("John val:", val)
-
-		var player map[string]any
-		err = json.Unmarshal([]byte(val), &player)
-		if err != nil {
-			fmt.Println("Error unmarshaling JSON:", err)
-			return
-		}
-		fmt.Println("John", player)
 
 		tmpl, err := template.ParseFiles(playerControlsPath)
 		if err != nil {
@@ -114,10 +101,49 @@ var ctx = context.Background()
 
 func redisClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "localhost:6969",
 		Password: "",
 		DB:       0, // use default DB
 	})
 
 	return rdb
+}
+
+func savePlayerInfo(player Player, rdb *redis.Client, status string) {
+	data, err := rdb.Get(ctx, player.Name).Result()
+	if err != nil {
+		playerJSON, err := json.Marshal(player)
+		if err != nil {
+			log.Println("Marshal err: ", err)
+		}
+
+		err = rdb.Set(ctx, player.Name, playerJSON, time.Duration(time.Minute*15)).Err()
+		if err != nil {
+			log.Println(err)
+		}
+
+		return
+	}
+
+	err = json.Unmarshal([]byte(data), &player)
+	if err != nil {
+		log.Println("UNmarshal err: ", err)
+		return
+	}
+
+	player.Status = status
+
+	playerJSON, err := json.Marshal(player)
+	if err != nil {
+		log.Println("Marshal err: ", err)
+		return
+	}
+
+	err = rdb.Set(ctx, player.Name, playerJSON, 0).Err()
+	if err != nil {
+		log.Println("Updating player status:", err)
+		return
+	}
+
+	log.Printf("Player %s %s", player.Name, player.Status)
 }
