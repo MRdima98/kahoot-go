b02e3ed72d2c240438adaec21cdb4384a552f086
dima <dimafrunza69@gmail.com>
2025-03-02 16:13:17 +0100

    Reading the question

---
+++ handlers/gameMaster.go
@@ -0,0 +1,86 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+var Answered = 0
+
+const (
+	index      = "index.html"
+	indexPath  = "templates/index.html"
+	headPath   = "templates/head.html"
+	footerPath = "templates/footer.html"
+)
+
+var tmpl = template.Must(
+	template.ParseFiles(
+		indexPath, footerPath, headPath,
+	),
+)
+
+type Question struct {
+	Quest   string `json:"question"`
+	Ans1    string `json:"answer1"`
+	Ans2    string `json:"answer2"`
+	Ans3    string `json:"answer3"`
+	Ans4    string `json:"answer4"`
+	Correct string `json:"correct"`
+	Pic     string `json:"path"`
+}
+
+func QuestionsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	answered = conn
+}
+
+func GameHandler(w http.ResponseWriter, r *http.Request) {
+	rdb := RedisClient()
+
+	var questions []Question
+
+	data, err := rdb.Get(context.Background(), Questions).Result()
+	if err != nil {
+		log.Println("We can't find them")
+	}
+
+	err = json.Unmarshal([]byte(data), &questions)
+	if err != nil {
+		log.Println("Can't unmarshal them data")
+	}
+
+	data, err = rdb.Get(context.Background(), "n_answered").Result()
+	if err != nil {
+		log.Println("We can't count")
+	}
+
+	Answered, err = strconv.Atoi(data)
+	if err != nil {
+		log.Println("Converting n_answered: ", err)
+	}
+
+	err = tmpl.ExecuteTemplate(w, index, struct {
+		Path     string
+		Answered int
+		Current  Question
+	}{
+		r.URL.Path,
+		Answered,
+		questions[curr_question],
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	rdb.Close()
+}
---
+++ handlers/gameState.go
@@ -0,0 +1,261 @@
+package handlers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/redis/go-redis/v9"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+const (
+	playerControlsPath = "templates/playerControls.html"
+	connected          = "connected"
+	disconnected       = "disconnected"
+	no_answer          = ""
+	Questions          = "questions"
+)
+
+var answered *websocket.Conn
+var curr_question = 0
+
+type Player struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+	Answer string `json:"answer"`
+	Score  int    `json:"score"`
+}
+
+type Options struct {
+	Ans1 string `json:"ans1"`
+	Ans2 string `json:"ans2"`
+	Ans3 string `json:"ans3"`
+	Ans4 string `json:"ans4"`
+}
+
+var colors = map[string]string{
+	"red_answer":    "bg-kahootRed",
+	"blue_answer":   "bg-kahootBlue",
+	"green_answer":  "bg-kahootGreen",
+	"yellow_answer": "bg-kahootYellow",
+}
+
+// Write down what the player is answering
+// the game socket will reset the answered and write down the points
+// also will clear the current answer and reset the gray
+func SocketHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("\n\nOpened PLAYER connection!")
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var player Player
+	rdb := RedisClient()
+
+	conn.SetCloseHandler(func(code int, text string) error {
+		if player == (Player{}) {
+			return errors.New("No player on this connection...somehow")
+		}
+
+		savePlayerInfo(player, rdb, disconnected)
+
+		rdb.Close()
+		return nil
+	})
+	var tmpl *template.Template
+
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		var result map[string]any
+		err = json.Unmarshal(p, &result)
+		if err != nil {
+			fmt.Println("Error unmarshaling JSON in for loop get:", err)
+			return
+		}
+
+		if result["ans1"] != nil {
+			whichAnswer("red_answer", rdb, tmpl, conn)
+		}
+
+		if result["ans2"] != nil {
+			whichAnswer("blue_answer", rdb, tmpl, conn)
+		}
+
+		if result["ans3"] != nil {
+			whichAnswer("green_answer", rdb, tmpl, conn)
+		}
+
+		if result["ans4"] != nil {
+			whichAnswer("yellow_answer", rdb, tmpl, conn)
+		}
+
+		if result["name"] != nil {
+			player = Player{
+				Name:   result["name"].(string),
+				Status: connected,
+				Answer: no_answer,
+				Score:  0,
+			}
+			savePlayerInfo(player, rdb, connected)
+
+			tmpl, err = template.ParseFiles(playerControlsPath)
+			if err != nil {
+				log.Println(err)
+			}
+
+			var tpl bytes.Buffer
+			err = tmpl.Execute(&tpl, readQuestion(rdb))
+			if err != nil {
+				log.Printf("template execution: %s", err)
+			}
+
+			if err := conn.WriteMessage(websocket.TextMessage, tpl.Bytes()); err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
+
+var ctx = context.Background()
+
+func RedisClient() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6969",
+		Password: "",
+		DB:       0, // use default DB
+	})
+
+	return rdb
+}
+
+func savePlayerInfo(player Player, rdb *redis.Client, status string) {
+	data, err := rdb.Get(ctx, player.Name).Result()
+	if err != nil {
+		playerJSON, err := json.Marshal(player)
+		if err != nil {
+			log.Println("Marshal err: ", err)
+		}
+
+		err = rdb.Set(ctx, player.Name, playerJSON, time.Duration(time.Minute*15)).Err()
+		if err != nil {
+			log.Println(err)
+		}
+
+		return
+	}
+
+	err = json.Unmarshal([]byte(data), &player)
+	if err != nil {
+		log.Println("UNmarshal err: ", err)
+		return
+	}
+
+	player.Status = status
+
+	playerJSON, err := json.Marshal(player)
+	if err != nil {
+		log.Println("Marshal err: ", err)
+		return
+	}
+
+	err = rdb.Set(ctx, player.Name, playerJSON, 0).Err()
+	if err != nil {
+		log.Println("Updating player status:", err)
+		return
+	}
+
+	log.Printf("Player %s %s", player.Name, player.Status)
+}
+
+func saveNAnswered(rdb *redis.Client) int {
+	n_answered := "n_answered"
+	tmp, err := rdb.Get(ctx, n_answered).Result()
+	if err != nil {
+		log.Println("Reading n_answered: ", err)
+	}
+
+	count, err := strconv.Atoi(tmp)
+	if err != nil {
+		log.Println("Converting n_answered: ", err)
+	}
+
+	count++
+
+	err = rdb.Set(ctx, n_answered, count, 0).Err()
+	if err != nil {
+		log.Println("Writing n_answered: ", err)
+	}
+
+	return count
+}
+
+func readQuestion(rdb *redis.Client) Question {
+	tmp, err := rdb.Get(ctx, Questions).Result()
+	if err != nil {
+		log.Println("Reading questions", err)
+	}
+
+	var options []Question
+
+	err = json.Unmarshal([]byte(tmp), &options)
+	if err != nil {
+		log.Println("Unmarshal err: ", err)
+	}
+
+	return options[curr_question]
+}
+
+func whichAnswer(answer string, rdb *redis.Client, tmpl *template.Template, conn *websocket.Conn) {
+	html := `
+			<div id="n_answered" hx-swap-oob="innerHTML">
+			%d
+			</div>
+			`
+	html = fmt.Sprintf(html, saveNAnswered(rdb))
+
+	if answered == nil {
+		fmt.Println("There is no open game")
+		return
+	}
+
+	if err := answered.WriteMessage(websocket.TextMessage, []byte(html)); err != nil {
+		fmt.Println("Can't sign that a player wrote a message", err)
+		return
+	}
+
+	var ans_button bytes.Buffer
+	err := tmpl.ExecuteTemplate(&ans_button, answer, readQuestion(rdb))
+	if err != nil {
+		log.Println(err)
+	}
+
+	gray := strings.ReplaceAll(ans_button.String(), colors[answer], "bg-gray-200")
+
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(gray)); err != nil {
+		log.Println(err)
+		return
+	}
+}
