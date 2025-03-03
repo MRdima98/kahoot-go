ded9e99a858969c14d88e7ab8e8d7e49b49fdde6
dima <dimafrunza69@gmail.com>
2025-03-03 06:53:51 +0100

    Timer

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,28 @@
 window.addEventListener("load", function () {
+  /** @type {HTMLImageElement} */
   const img = document.getElementById("test");
 
-  if (img != null) {
-    if (img.naturalWidth < img.naturalHeight) {
-      img.style.width = "600px";
-    } else {
-      img.style.width = "1800px";
-    }
+  if (img.naturalWidth < img.naturalHeight) {
+    img.style.width = "600px";
+  } else {
+    img.style.width = "1800px";
   }
+
+  /** @type {HTMLDivElement} */
+  const timer = document.getElementById("timer");
+
+  var start = Date.now();
+  setInterval(function () {
+    /** @type {Number} */
+    let delta = Date.now() - start;
+    /** @type {Number} */
+    let countDown = 30;
+    countDown -= Math.floor(delta / 1000);
+
+    if (countDown <= 0) {
+      // when all is done, you shall make a quick post to the server
+      return;
+    }
+    timer.innerHTML = countDown;
+  }, 1000);
 });
